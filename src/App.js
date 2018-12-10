11b45651399593570eb6d80e1adc55e5b31b3101
imannie <tranntphuong@gmail.com>
2018-12-10 15:06:13 -0800

    second commit

---
+++ src/App.js
@@ -0,0 +1,38 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+  /*state = {
+    data: [],
+  }
+
+  componentDidMount = () => {
+    this.onFetch();
+  }*/
+  
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+          <h1 className="App-title">Welcome to React</h1>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </header>
+      </div>
+    );
+  }
+}
+
+
+export default App;
