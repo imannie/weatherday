10d9501131339ab20157df36b7b22692fb4a16a5
imannie <tranntphuong@gmail.com>
2018-12-11 17:17:40 -0800

    fetch is calling

--- src/App.js
+++ src/App.js
@@ -1,46 +1,63 @@
-import React, { Component } from 'react';
+import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-class App extends Component {
+import Titles from "./components/Titles";
+import Form from "./components/Form";
+import Weather from "./components/Weather.js";
+
+const API_KEY = "3014d36ee8f7c35e8cd52b7c9c040e04";
+
+
+class App extends React.Component {
+  //constructor() {
+    //this.getWeather = this.getWeather.bind(this);
   state = {
-    
+    temperature: undefined,
+    city: undefined,
+    country: undefined,
+    humidity: undefined,
+    description: undefined,
+    error: undefined
   }
+    getWeather =  async (e) => {
+      e.preventDefault();
+      const city = e.target.elements.name.city.value;
+      const country = e.target.elements.name.country.value;
+      const api_call = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city},${country}&APPID=${API_KEY}`);
 
-  onFetch() {
-    console.log('Fetch calling');
-    fetch('http://api.openweathermap.org/data/2.5/weather?q=${this.state.searchBox},California&appid=0de82b6b4ba5d843dac44bbee4d02543')
-    .then(response => respone.json())
-    .then(data => {
-      console.log('got data -', data);
+      const data = await api_call.json();
+      console.log(data);
       this.setState({
+        temperature: data.main.temp,
+        city: data.name,
+        country: data.sys.country,
+        humidity: data.main.humidity,
+        description: data.weather[0].description,
+        error: ""
 
-      });
+      })
     }
-  }
-  
+ 
   render() {
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <h1 className="App-title">Welcome to React</h1>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+          <Titles />
+          <Form getWeather={this.getWeather}/>
+          <Weather 
+            temperature={this.state.temperature}
+            city={this.state.city}
+            country={this.state.country}
+            humidity={this.state.country}
+            error={this.state.error}
+            />
         </header>
       </div>
     );
   }
-}
+};
 
 
 export default App;
---
+++ src/components/Titles.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+class Titles extends React.Component {
+    render() {
+        return (
+            <div>
+                <h1>Weather Looks</h1>
+                <p>Tell me the weathetr today..</p>
+            </div>
+        );
+    }
+};
+
+
+export default Titles;
