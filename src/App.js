e8556a63e99b11b5172a36afe4a2fe97924b90f8
imannie <tranntphuong@gmail.com>
2018-12-11 18:11:17 -0800

    fixed Weather component

--- src/App.js
+++ src/App.js
@@ -22,21 +22,31 @@ class App extends React.Component {
   }
     getWeather =  async (e) => {
       e.preventDefault();
-      const city = e.target.elements.name.city.value;
-      const country = e.target.elements.name.country.value;
-      const api_call = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city},${country}&APPID=${API_KEY}`);
+      const city = e.target.elements.city.value;
+      const country = e.target.elements.country.value;
+      const api_call = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city},${country}&APPID=${API_KEY}&units=`);
 
       const data = await api_call.json();
-      console.log(data);
-      this.setState({
-        temperature: data.main.temp,
-        city: data.name,
-        country: data.sys.country,
-        humidity: data.main.humidity,
-        description: data.weather[0].description,
-        error: ""
-
-      })
+      if (city && country) {
+        console.log(data);
+        this.setState({
+          temperature: data.main.temp,
+          city: data.name,
+          country: data.sys.country,
+          humidity: data.main.humidity,
+          description: data.weather[0].description,
+          error: ""
+        });
+      } else {
+        this.setState({
+          temperature: undefined,
+          city: undefined,
+          country: undefined,
+          humidity: undefined,
+          description: undefined,
+          error: "please enter the value."
+        });
+      }
     }
  
   render() {
--- src/components/Weather.js
+++ src/components/Weather.js
@@ -1,16 +1,16 @@
 import React from "react";
 
-class Weather extends React.Component {
-    render() {
-        return (
-            <div>
-                { this.props.city && this.props.country && <p>Location: { this.props.city }, { this.props.country }</p> }
-                { this.props.temperature && <p>Temperature: { this.props.temperature }</p> }
-                { this.props.humidity && <p>Humidity: { this.props.humidity }</p> }
-                { this.props.description && <p>Conditions: { this.props.description }</p> }
-            </div>
-        )
-    }
+const Weather = (props) => {
+    return (
+        <div>
+            { props.city && props.country && <p>Location: { props.city }, { props.country }</p> }
+            { props.temperature && <p>Temperature: { props.temperature }</p> }
+            { props.humidity && <p>Humidity: { props.humidity }</p> }
+            { props.description && <p>Conditions: { props.description }</p> }
+            { props.error && <p>{ props.error }</p> }
+        </div>
+    );
 }
+
 
 export default Weather;
